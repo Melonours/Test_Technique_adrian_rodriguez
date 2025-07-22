d690cdae39c871e5898f474a80f54096849266e7
Melonours <adrianrumi@hotmail.com>
2025-07-23 01:48:04 +0200

    command line user frendly finie, fonctions de recherche sur wikipédia ok

--- app.py
+++ app.py
@@ -1,22 +1,57 @@
-#import des librairies utilisées
+#Import des librairies utilisées
 import sqlite3
 import wikipedia
 
+#Set up de la langue par défaut de wikipédia en français
+wikipedia.set_lang("fr")
 
 
-commands = ["exit", "help", "joe", "search"]
+#Fonctions utilisées dans l'app
 
-print('Bienvenue sur l\'application ! \n \n"help" : afin de voir les commandes disponibles')
+#Fonction d'insertion dans la DB
+
+
+#Fonction de recherche du nom demandé par l'utilisateur sur wikipédia
+def validate(user_request):
+    value = wikipedia.search(user_request, results= 1)
+    print(value)
+    joe = wikipedia.page(value)
+    print(joe.title, joe.summary)
+
+#Fonction de lancement de la recherche et de suggestion en cas de mauvaise typo
+def search(answer): 
+    validate_list = ["Yes", "Oui", "Y", "O", ""]
+
+    search_name = wikipedia.suggest(answer)
+
+    validate_search = input(f"Did you mean {search_name.title()} ?").capitalize()
+
+    if validate_search in validate_list:
+            
+        validate(search_name)
+
+
+
+#Variables globales
+user_ans = ""
+
+commands = ["exit", "help", "search"]
+
+
+#Début de l'app
+print('Welcome to the app ! \n \nType "help" to see more commands')
 
 while True : 
-    cmd = input("\n > 2")
+    cmd = input("\n >>")
     if cmd == "exit":
         print("bye")
         break
     if cmd == "help":
-        print("voici la liste des commandes disponibles : ")
+        print(f"\nHere are the available commands : {commands} ")
     if cmd not in commands: 
-        print('commande non connue \n"help" : pour voir les commandes disponibles')
+        print('\nUnknown command, type "help" to see the available commands')
     if cmd == "search":
-        user_ans = input("veuillez entrez le nom de la personne recherchée : \n ")
-        
+        user_ans = input("\nPlease type the name of the person you're looking for : \n")
+        search(user_ans)
+
+
