256d7ad39a6402ed0617467a167641f2b6f0ba9a
Melonours <adrianrumi@hotmail.com>
2025-07-24 19:58:45 +0200

    changement des fonctions, recherche sur wikipédia fonctionnelle à 90%

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #Import des librairies utilisées
+from os import error
 import sqlite3
 import wikipedia
 
 #Set up de la langue par défaut de wikipédia en français
-wikipedia.set_lang("fr")
+# wikipedia.set_lang("fr")
 
 
 #Fonctions utilisées dans l'app
@@ -12,37 +13,70 @@ wikipedia.set_lang("fr")
 
 
 #Fonction de recherche du nom demandé par l'utilisateur sur wikipédia
-def validate(user_request):
-    value = wikipedia.search(user_request, results= 1)
-    print(value)
-    joe = wikipedia.page(value)
-    print(joe.title, joe.summary)
+# def validate(user_request):
+#     print(user_request)
+#     value = " "
+#     value = wikipedia.search(user_request, results = 1)
+#     print(value)
+#     joe = ""
+#     joe = wikipedia.page(title = value)
+#     print(joe.title)
+
+#     try :
+#         print(joe.summary)
+#     except wikipedia.exceptions.DisambiguationError as e :
+#         print(e.options)
+
+
 
 #Fonction de lancement de la recherche et de suggestion en cas de mauvaise typo
-def search(answer): 
-    validate_list = ["Yes", "Oui", "Y", "O", ""]
+# def search(answer): 
+#     validate_list = ["Yes", "Oui", "Y", "O", ""]
 
-    search_name = wikipedia.suggest(answer)
+#     search_name = wikipedia.suggest(answer)
+#     print(str(search_name))
 
-    validate_search = input(f"Did you mean {search_name.title()} ?").capitalize()
+#     if search_name == None :
+#         mex = wikipedia.search(answer)
+#         print(mex)
 
-    if validate_search in validate_list:
-            
-        validate(search_name)
+#     validate_search = input(f"Did you mean {search_name} ?").capitalize()
 
+#     if validate_search in validate_list:
+#         print(search_name)
+#         validate(search_name)
+#     else :
+#         print("joe")
 
+def validate(user_validation):
+    
+
+def search(answer):
+    rep = wikipedia.search(answer, results = 1)
+    print(rep)
+
+    if rep == []:
+        print("I don't know this person \n")
+        rep = wikipedia.suggest(answer).title()
+        input(f"Did you mean {rep} ?")
+
+    validate()
+
+    person = wikipedia.page(rep, auto_suggest=False)
+    print(person.title)
 
 #Variables globales
 user_ans = ""
 
 commands = ["exit", "help", "search"]
 
+validate_list = ["Yes", "Oui", "Y", "O", ""]
 
 #Début de l'app
 print('Welcome to the app ! \n \nType "help" to see more commands')
 
 while True : 
-    cmd = input("\n >>")
+    cmd = input("\n >>").strip()
     if cmd == "exit":
         print("bye")
         break
